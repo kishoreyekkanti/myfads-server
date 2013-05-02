af83f67e7ba4f4b3fe3086b378ab5a7864aaafee
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2013-05-02 22:46:53 +0530

    Initial project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyfadsServer::Application.config.secret_key_base = '19b45095a445e84dfcf8e60b1d8ccd64c92b9fe53f9988fd8dae965d4dd34e29c2bf30225e5d80d97c36ec26547d9d813ba4f8e0da18a2fb5652e9c684d90408'
